9f0082e3277f250b45feeff5fd57704b2b3e5ccd
Rens <rensvelden@gmail.com>
2021-11-02 14:58:05 +0100

    Test heartbeat chart toegevoegd
    De test heartbeat lijn grafiek is toegevoegd, het is een functie.

--- Sleepwell/Sleepwell/Form1.cs
+++ Sleepwell/Sleepwell/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Sleepwell
 {
@@ -15,6 +16,22 @@ namespace Sleepwell
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreateChart();          
+        }
+
+        private void CreateChart()
+        {
+            //voor deze methode moet er een chart element toegevoegd worden met de naam chrtHeartBeat
+            var series = new Series("Heartbeat");
+            //chart type veranderen naar lijn
+            series.ChartType = SeriesChartType.Line;
+            //de lijn zelf toevoegen
+            series.Points.DataBindXY(new[] { 1.00, 2.00, 3.00, 4.00 }, new[] { 50, 56, 45, 60 });
+            chrtHeartbeat.Series.Add(series);
         }
     }
 }
