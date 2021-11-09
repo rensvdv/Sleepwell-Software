06057aa644e285986813a5a0a92f4ab6a6b4d581
Unknown <Jaibreyon.lourens@gmail.com>
2021-11-09 10:21:26 +0100

    connection to arduino
    added the connection to the arduino

--- Sleepwell/Sleepwell/Form4.Designer.cs
+++ Sleepwell/Sleepwell/Form4.Designer.cs
@@ -29,10 +29,14 @@ namespace Sleepwell
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dataTimer = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,23 +46,39 @@ namespace Sleepwell
             this.chart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Location = new System.Drawing.Point(9, 10);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(385, 214);
+            this.chart1.Size = new System.Drawing.Size(289, 174);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
             // 
+            // dataTimer
+            // 
+            this.dataTimer.Interval = 1000;
+            this.dataTimer.Tick += new System.EventHandler(this.DataTimer_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 199);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(286, 134);
+            this.listBox1.TabIndex = 1;
+            // 
             // Form4
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.chart1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form4";
             this.Text = "Form4";
             this.Load += new System.EventHandler(this.Form4_Load);
@@ -70,5 +90,8 @@ namespace Sleepwell
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer dataTimer;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
--- Sleepwell/Sleepwell/Form4.cs
+++ Sleepwell/Sleepwell/Form4.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 using EASendMail;
 using MySql.Data.MySqlClient;
+using System.IO.Ports;
 
 namespace Sleepwell
 {
@@ -18,6 +19,10 @@ namespace Sleepwell
         string naam;
         int leeftijd;
         string slaaptijd;
+        bool isConnected = false;
+
+        string pulse;
+        SerialPort port;
         public Form4(string naam, int leeftijd, string slaaptijd)
         {
             InitializeComponent();
@@ -28,9 +33,43 @@ namespace Sleepwell
 
         private void Form4_Load(object sender, EventArgs e)
         {
+            ConnectToArduino();
+
+            if (isConnected)
+            {
+                dataTimer.Start();
+            }
+        }
+
+        private void ConnectToArduino()
+        {
+            isConnected = true;
+            string selectedPort = "COM6";
+            try
+            {
+                port = new SerialPort(selectedPort, 9600, Parity.None, 8, StopBits.One);
+                port.Open();
+            }catch(Exception es)
+            {
+                MessageBox.Show("Failed to connect");
+            }
+            
 
         }
 
+        public string GetData(string data)
+        {
+            //all commands to the port consist of # command and \n
+            // a command has 4 letters
+            port.Write("#DATA\n");
+            data = port.ReadExisting();
+            if(data != "")
+            {
+                listBox1.Items.Add(data);
+            }
+            
+            return data;
+        }
         private void CreateChart()
         {
             //voor deze methode moet er een chart element toegevoegd worden met de naam chrtHeartBeat
@@ -42,6 +81,12 @@ namespace Sleepwell
             series.Points.DataBindXY(new[] { 1.00, 2.00, 3.400, 4.00 }, new[] { 50, 56, 45, 60 });
             //chrtHeartbeat.Series.Add(series);
 
+        }
+
+        private void DataTimer_Tick(object sender, EventArgs e)
+        {
+            GetData(pulse);
+            
         }
     }
 }
